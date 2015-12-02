4ffef9d634f03d4179ea73ab388a623cbe29ded8
Guillermo Alvarez <alvarezgui546@gmail.com>
2015-12-02 11:32:08 -0500

    shuffling around code

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include <assimp/postprocess.h>
 #include <utils/io/stb_image.h>
 #include <skybox/skybox.h>
+#include <platform/glfw_launcher.h>
 
 #define MESH_FILE "/home/alvaregd/Documents/Games/greyscale_terrain.obj"
 #define SKY_BACK "/home/alvaregd/Documents/Games/water_reflection/assets/back.png"
@@ -198,10 +199,9 @@ int main () {
     //start logger system
     assert(restart_gl_log());
 
-    hardware = {}; //must initialize window before starting gl stuff
-
+    Hardware hardware;
     //create our main window
-    assert(start_gl());
+    assert(start_gl(&hardware));
 
     GLuint meshVao;
     int pointCount;
@@ -328,7 +328,7 @@ int main () {
     mat4 skyViewMatrix;
 
     while (!glfwWindowShouldClose (hardware.window)) {
-        updateMovement(&camera);
+        updateMovement(&camera, &input);
         calculateViewMatrices(&camera);
         //set the new view matrix @ the shader level
 
@@ -346,8 +346,6 @@ int main () {
 
         //draw the sky box
         glUseProgram(skybox_shader_program);
-
-
 
         //calculate pitch
         angle += 0.001f;
@@ -372,9 +370,6 @@ int main () {
         }
         glfwSwapBuffers(hardware.window);
     }
-
-
-
 
 
     /* close GL context and any other GLFW resources */
@@ -545,63 +540,6 @@ static void calculateViewMatrices(Camera *camera){
     camera->viewMatrix = camera->Rpitch * camera->Ryaw * camera->T;
 }
 
-
-/**
- * Calculate the player's kinematics and render it
- * When we detect a keypress, push becomes 1 (positive acceleration) until we release the key.
- * When key releases pushing becomes -1 (negative acceleration) to indicate that we are slowing down.
- */
-static void updateMovement(Camera* camera) {
-
-    //if any of the WASD keys are pressed, the camera will move
-    if(input.wPressed || input.sPressed || input.aPressed || input.dPressed) {
-        camera->pushing = 1;
-    }
-
-    //while we push,
-    if (camera->pushing) {
-        //set linear motion
-        const double maxVelocity = 0.1 * (camera->pushing> 0);
-        const double acceleration= camera->pushing>0 ? 0.2:0.1;
-
-        if(camera->move_angle == 90.0f || camera->move_angle == -90.0f) {
-            //Player has pressed either straf left or straf right, calculate the direction Vector using cross product
-            //of actor's heading and the up direction
-            vec3 left = cross(vec3(camera->viewMatrix.m[2],camera->viewMatrix.m[6],camera->viewMatrix.m[10]),
-                              vec3(camera->viewMatrix.m[1],camera->viewMatrix.m[5],camera->viewMatrix.m[9]));
-
-            //update the camera's velocity accordingly
-            camera->velocity.v[0] =(float)(camera->velocity.v[0] * (1-acceleration) +
-                                           ( left.v[0]) * ((camera->move_angle == 90 )? 1:-1) * (acceleration *maxVelocity));
-            camera->velocity.v[2] =(float)(camera->velocity.v[2] * (1-acceleration) +
-                                           ( left.v[2]) * ((camera->move_angle == 90 )? 1:-1) * (acceleration *maxVelocity));
-        }else{
-            //player has hit forward (w) or backwards (s). update the velocity in these directions
-            camera->velocity.v[0] =(float)(camera->velocity.v[0] * (1-acceleration) +
-                                           ( camera->viewMatrix.m[2]) * ((camera->move_angle == 180 )? -1:1) * (acceleration *maxVelocity));
-            camera->velocity.v[1] =(float)(camera->velocity.v[1] * (1-acceleration) +
-                                           ( camera->viewMatrix.m[6]) * ((camera->move_angle == 180 )? -1:1) * (acceleration *maxVelocity));
-            camera->velocity.v[2] =(float)(camera->velocity.v[2] * (1-acceleration) +
-                                           ( camera->viewMatrix.m[10]) * ((camera->move_angle == 180 )? -1:1) * (acceleration *maxVelocity));
-        }
-        camera->moving = true;
-    }
-
-    //while we are moving (velocity is nonzero), update the camera's position
-    if (camera->moving) {
-        camera->pos[0] += -camera->velocity.v[0] *0.02f;
-        camera->pos[2] += -camera->velocity.v[2] *0.02f;
-        camera->pos[1] += -camera->velocity.v[1] *0.02f;
-        if(dot(camera->velocity,camera->velocity) < 1e-9) {
-            camera->velocity.v[0] = camera->velocity.v[2] = camera->velocity.v[1] = 0.0f;
-            camera->pushing = 0;
-            camera->moving = false;
-        }
-    }
-    if(camera->pushing){
-        camera->pushing = -1;
-    }
-}
 
 
 
--- main.h
+++ main.h
@@ -1,7 +1,6 @@
 //
 // Created by alvaregd on 27/11/15.
 //
-
 #ifndef SIMPLE_LEVEL_EDITOR_MAIN_H_H
 #define SIMPLE_LEVEL_EDITOR_MAIN_H_H
 
@@ -13,12 +12,11 @@
 #include <utils/math_utils/maths_funcs.h>
 #include <utils/math_utils/quat_funcs.h>
 #include <utils/log_utils/logger.h>
-#include <platform/glfw_launcher.h>
-#include <platform/hardware.h>
 #include <utils/io/shader_loader.h>
 #include <array>
 #include "onScreenObjects.h"
 #include <vector>
+#include "camera/camera.h"
 
 #define VERTEX_SHADER "/home/alvaregd/Documents/Games/simple_level_editor/shaders/grid.vert"
 #define FRAGMENT_SHADER "/home/alvaregd/Documents/Games/simple_level_editor/shaders/grid.frag"
@@ -30,27 +28,6 @@ enum State{
     STATE_ORIENTATION
 };
 
-struct Camera{
-
-    float pos[3]; // don't start at zero, or we will be too close
-    float yaw = 0.0f; // y-rotation in degrees
-    float pitch = 0.0f;
-    float signal_amplifier = 0.1f;
-    mat4 T;
-    mat4 Rpitch;
-    mat4 Ryaw;
-    mat4 viewMatrix;
-
-    GLint view_mat_location;
-    GLint proj_mat_location;
-
-    int pushing; //-1 slowing down, +1 accelerating , 0 = idle
-    bool moving; //velocity != 0
-    double move_angle;
-
-    vec3 velocity; //actor's velocity
-};
-
 
 struct Input{
     bool wPressed;
@@ -59,20 +36,17 @@ struct Input{
     bool dPressed;
 };
 
-
 static Grid grid;
 static Camera camera;
 static Input input;
-Hardware hardware;
 static State state;
 
 static void cursor_position_callback(GLFWwindow *window, double xpos, double ypos);
 static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods);
 static void calculateViewMatrices(Camera *camera);
-static void updateMovement(Camera* camera);
 static void updateGridHeight(Grid* grid);
 
 void createVertexBufferObject(GLuint *name, size_t size, GLfloat *data);
 void createVertexArrayObjet(GLuint* name, GLuint* bufferObject, GLint dimensions);
 void setColourMesh(GLuint* vao, GLuint* bufferObject, GLint dimensions, GLuint* attributeIndex);
-#endif //SIMPLE_LEVEL_EDITOR_MAIN_H_H
+#endif //SIMPLE_LEVEL_EDITOR_`_H_H
